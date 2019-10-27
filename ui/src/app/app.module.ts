6b6972a2697ad8bce33304544845c7f979939fe7
sumguy002 <bryanwu123456@gmail.com>
2019-10-26 18:01:47 -0700

    semester Component

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SemesterComponent } from './semester/semester.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SemesterComponent
   ],
   imports: [
     BrowserModule
